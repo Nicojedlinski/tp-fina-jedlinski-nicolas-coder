0adfb9661f89c0235d605f4209ec86a40a12d775
nicojedo tp final <nicolasjedlinski@gmail.com>
2023-08-06 15:06:36 -0300

    js con condicional y bucles

---
+++ js/main.js
@@ -0,0 +1,74 @@
+let edad= 23
+//edad=30
+let nombre= " "
+
+//let Bienvenido/a= prompt ("Welcome") 
+
+//alert ("Bienvenido" + "  " + usuario)
+
+//edad=prompt("que edad tienes ?")
+
+//alert ("eres mayor de 18 ?")
+
+//edad=prompt("si o no ?")
+
+let usuario=prompt ("Ingresa tu Nombre");
+
+if(usuario= " "){
+    console.log("no ingresaste tu usuario");
+}else{
+    console.log("Bienvenido/a"  + usuario);
+}
+
+alert ("Bienvenido/a. Responda las siguientes consultas para poder enviarle la propuesta")
+
+let legal= 18
+
+let mail=prompt ("Ingresa un mail");
+
+if(usuario= " "){
+    console.log("no ingresaste tu usuario");
+}else{
+    console.log("Bienvenido/a"  + mail);
+}
+
+//if(legal <18){
+    //console.log("sos menor.No podes adquirir los servicios.Consultalo con un mayor");
+//}else{
+    //console.log("sos mayor. Podes adquirir los servicios");
+//}
+
+let num = parseFloat(prompt("ingresa tu edad"))
+    let mayoredad=num >18;
+    console.log(mayoredad);
+
+if(mayoredad <=18){
+    console.log("sos mayor. Podes adquirir los servicio");
+}else{
+    console.log("sos menor.No podes adquirir los servicios.Consultalo con un mayor");
+}
+
+let si=prompt ("Deseas contratar los servicios");
+
+if(si= "si"){
+    console.log("Gracias por elegirnos" + usuario + "en breve se comunicaran con vos");
+}
+
+//alert ("Gracias Por tu respuesta, en breve se comunicaran con vos")
+
+
+alert("Pudiste responder todo?")
+alert("Si")
+
+for(let i=0; i<1; i++){
+    let pregunta=prompt(" ")
+    if (pregunta ="no"){
+        respuesta="Responda Nuevamente el cuestionario"
+        alert(respuesta)
+    }else{
+        respuesta= "Pronto se le enviaran las opciones disponibles a su casilla de corre. puede ser enviado a su spam"
+        alert(respuesta)
+    }
+}
+
+alert ("En breve se comunicaran con vos. Atte el equipo Nico's")
